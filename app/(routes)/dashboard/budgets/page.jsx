0f997f1d63d95146826029651a8c6c751338bf9f
Anshita Srivastava <as1840@srmist.edu.in>
2025-02-07 10:33:07 +0530

    Updated budget page UI and logic

--- app/(routes)/dashboard/budgets/page.jsx
+++ app/(routes)/dashboard/budgets/page.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import BudgetList from './_components/BudgetList';
+export const dynamicConfig = 'force-dynamic';
+
+import dynamic from 'next/dynamic';
+
+const BudgetList = dynamic(() => import('./_components/BudgetList'), { ssr: false });
 
 function Budget() {
   return (
     <div className='p-10'>
-        <h2 className='font-bold text-3xl'>
-            My Budgets
-        </h2>
-        <BudgetList/>
+      <h2 className='font-bold text-3xl'>My Budgets</h2>
+      <BudgetList />
     </div>
-  )
+  );
 }
 
-export default Budget
+export default Budget;
